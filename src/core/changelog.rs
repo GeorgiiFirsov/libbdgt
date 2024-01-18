edb1cde3c1c5707cda9cebb896279b645cde15c1
Georgy Firsov <gfirsov007@gmail.com>
2024-01-18 21:17:36 +0000

    chore: initial implementation of changelogs

--- src/core/changelog.rs
+++ src/core/changelog.rs
@@ -15,32 +15,72 @@ pub(crate) struct SimpleChangelog<T> {
 }
 
 
+impl<T> SimpleChangelog<T> {
+    fn new() -> Self {
+        SimpleChangelog::<T> {
+            added: Vec::new(),
+            changed: Vec::new(),
+            removed: Vec::new()
+        }
+    }
+}
+
+
 /// Database changelog representation.
 pub(crate) struct Changelog {
-    /// Diff for accounts.
+    /// Accounts changelog.
     pub accounts: SimpleChangelog<Account>,
 
-    /// Diff for categories.
+    /// Categories changelog.
     pub categories: SimpleChangelog<Category>,
 
-    /// Diff for transactions.
+    /// Transactions changelog.
     pub transactions: SimpleChangelog<Transaction>,
 
-    /// Diff for plans.
+    /// Plans changelog.
     pub plans: SimpleChangelog<Plan>,
 }
 
 
 impl Changelog {
-    pub(crate) fn new(_binary_changelog: &[u8]) -> Result<Self> {
-        todo!("parse binary changelog")
+    /// Creates a new changelog object from binary representation.
+    /// 
+    /// * `binary_changelog` - binary changelog representation
+    pub(crate) fn new(binary_changelog: &[u8]) -> Result<Self> {
+        let result = Changelog {
+            accounts: SimpleChangelog::new(),
+            categories: SimpleChangelog::new(),
+            transactions: SimpleChangelog::new(),
+            plans: SimpleChangelog::new()
+        };
+
+        let mut changelog = std::io::Cursor::new(binary_changelog);
+        while let Some(_record) = Self::read_record(&mut changelog)? {
+            todo!("Implement record handling")
+        }
+
+        Ok(result)
     }
 
-    pub(crate) fn append(&mut self, changelog: Changelog) -> Result<()> {
+    /// Appends another changelog to the current one.
+    /// 
+    /// * `changelog` - a changelog to append
+    pub(crate) fn append(&mut self, _changelog: Changelog) -> Result<()> {
         todo!("append new changelog")
     }
 
+    /// Converts current changelog into a binary representation.
     pub(crate) fn as_bytes(&self) -> &[u8] {
         todo!("convert to bytes")
+    }
+}
+
+
+impl Changelog {
+    fn read_record<Br: std::io::BufRead>(binary_changelog: &mut Br) -> Result<Option<()>> {
+        let mut buffer = Vec::new();
+        binary_changelog.read_until(0x00, &mut buffer)?;
+
+        Ok(None)
     }
 }
